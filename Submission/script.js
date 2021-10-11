1394fa5223dd6733798cd03e1b23a766fc2c79af
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2021-10-11 15:57:35 +1300

    third level response

--- Submission/script.js
+++ Submission/script.js
@@ -6,10 +6,12 @@
  */
 
 let level = 1;
+let dumb = 0;
 
 const getBotReply = (msg) => {
   let name;
   let age;
+  let annual;
   if (
     (level === 1 && msg.toLowerCase() === "sarah connor") ||
     (level === 1 && msg.toLowerCase() === "john connor")
@@ -23,11 +25,44 @@ const getBotReply = (msg) => {
     name = msg;
     return `Nice to meet you ${name}. I'm Invest-o-bot (not a financial advisor). I'd like to assist you with some investment advice. First I will need some information about you. To start, how old are you?`;
   }
-  if (level === 2) {
+  if (level === 2 && msg >= 0 && msg < 130) {
     level = 3;
     // assign age variable
     age = msg;
     return `${age}, great, and what is your average annual income?`;
+  }
+  if (level === 2) {
+    age = msg;
+    dumb++;
+    return `huh? ${age}?? I said tell me HOW OLD YOU ARE.`;
+  }
+  // converts dollar amount to number if user inputs $ symbol at start of string
+  if (msg.charAt(0) === "$") {
+    msg = msg.substr(1);
+  }
+  if (level === 3 && msg >= 0 && msg <= 20000) {
+    level = 4;
+    // assign annual variable
+    annual = msg;
+    return `$${annual}, hmm... maybe consider talking with a financial advise bot first and coming back later. How much are you able to save per week?`;
+  }
+  if (level === 3 && msg < 100000) {
+    level = 4;
+    // assign annual variable
+    annual = msg;
+    return `$${annual}, great! how much of that are you able to save per week?`;
+  }
+  if (level === 3 && msg >= 100000) {
+    level = 4;
+    // assign annual variable
+    annual = msg;
+    return `$${annual}? congratulations! you've come to the right place. how much of that are you able to save per week?`;
+  }
+  if (level === 3) {
+    // assign annual variable
+    annual = msg;
+    dumb++
+    return `$${annual} is not an income, try typing a real number without symbols or commas`;
   }
 
   return "Error unknown...";
--- test/script.test.js
+++ test/script.test.js
@@ -25,9 +25,12 @@ describe("getBotReply", () => {
       "Nice to meet you Jared. I'm Invest-o-bot (not a financial advisor). I'd like to assist you with some investment advice. First I will need some information about you. To start, how old are you?";
     expect(botReply1).toEqual(expectedReply1);
     const botReply2 = getBotReply("31");
-    const expectedReply2 =
-      "31, great, and what is your average annual income?";
+    const expectedReply2 = "31, great, and what is your average annual income?";
     expect(botReply2).toEqual(expectedReply2);
+    const botReply3 = getBotReply("50000");
+    const expectedReply3 =
+      "$50000, great! how much of that are you able to save per week?";
+    expect(botReply3).toEqual(expectedReply3);
   });
   // it("should show window alert and refresh page", () => {
   //   const botReply1 = getBotReply("Sarah Connor");
